48dd6f516e5fcf6335691fe3d2ba8ccb15db2a1b
Amitosh Kumar <amitoshkumar10@gmail.com>
2021-10-27 10:44:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -20,16 +20,10 @@ import React, { useEffect,useState} from 'react';
         profileName: "Default",
         containerId: "secuuthForm", 
       onSubmit: (tokens) => {
-        console.log(tokens)
-         fetch('http://127.0.0.1:5000/', {
-        method: 'POST',
-        headers: {
-                  'Content-Type': 'application/json',
-                },
-       body: JSON.stringify(tokens).toString()});  
-  
-        
-      },
+
+        //Logic To send token to backend
+       
+  },
     });
     });
   };
